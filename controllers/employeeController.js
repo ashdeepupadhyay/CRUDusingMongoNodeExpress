2f8bff671a44f4d569d906f4e81bf0da095787c9
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-01 19:49:39 +0530

    inserting a record

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -2,13 +2,38 @@ const express = require('express');
 
 var router = express.Router();
 
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
+
 router.get('/',(req,res)=>{
     res.render('employee/addOrEdit',{
         viewTitle:"Insert Employee"
     });
 });
 
-router.post('/',(req,res)=>{
-    console.log(req.body);
+router.get('/list',(req,res)=>{
+   res.json('from list');
 });
+
+router.post('/',(req,res)=>{
+    insertRecords(req,res);
+});
+
+function insertRecords(req,res){
+    var employee = new Employee();
+    employee.fullName = req.body.fullName;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.city = req.body.city;
+
+    employee.save((err,doc)=>{
+        if(!err){
+            console.log("record inserted");
+            res.redirect('employee/list');
+        }
+        else{
+            console.log('error during record insertion :'+err);
+        }
+    });
+}
 module.exports = router;
