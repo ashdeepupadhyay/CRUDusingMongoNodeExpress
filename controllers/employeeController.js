ad88c5e2c1616bc9661ed70d1e7544d09aae33e5
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-01 20:13:47 +0530

    adding the form validations

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -32,8 +32,30 @@ function insertRecords(req,res){
             res.redirect('employee/list');
         }
         else{
+                if(err.name== 'ValidationError'){
+                    handleValidationError(err,req.body);
+                    res.render('employee/addOrEdit',{
+                        viewTitle : "Insert Employee",
+                        employee: req.body
+                    })
+                }
             console.log('error during record insertion :'+err);
         }
     });
+}
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
 }
 module.exports = router;
